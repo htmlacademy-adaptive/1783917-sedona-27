fe4d8579a09d540f53317480ca3bb3c4560e60e7
Elena Smirnova <erendakova@mail.ru>
2023-01-24 19:10:10 +0300

    вносит изменения

--- gulpfile.js
+++ gulpfile.js
@@ -136,8 +136,8 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
-  gulp.watch('source/js/*.js').on('change', browser.reload);
+  gulp.watch('source/*.html',gulp.series(html,reload));
+  gulp.watch('source/js/*.js',gulp.series(scripts,reload));
 }
 
 //Build
